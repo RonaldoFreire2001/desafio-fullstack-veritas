d0386a382f2637037c6c22c6f15212725288e4d3
Ronaldo Freire <RonaldoFreire2001>
2025-11-09 21:51:11 -0300

    Finalizando projeto com todas as correções de estilo e bônus

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
-// Importa o React e o nosso quadro Kanban
 import React from 'react';
 import KanbanBoard from './components/KanbanBoard';
+import './App.css'; // <-- ESTA LINHA É O CONSERTO!
 
-// A "função" App é o nosso componente principal
 function App() {
-  // "return (...)" é o que o componente "desenha" na tela
   return (
+    // A classe "App" agora vai ser lida pelo App.css
     <div className="App">
-      {/* Um cabeçalho simples */}
       <header>
         <h1>Meu Mini Kanban</h1>
       </header>
       
-      {/* Aqui nós mandamos o React desenhar o nosso quadro */}
       <KanbanBoard />
     </div>
   );
 }
 
-// Exporta o App para que o "index.js" (o porteiro) possa usá-lo
 export default App;
